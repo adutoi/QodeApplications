43d065ee8be5e4d3d1d9a1cd6c91cd7e3c04ed67
Anthony D. Dutoi <adutoi@medusa>
2023-12-20 20:44:59 -0800

    core antisymm routine cleaned up and ready for wrapping ... driver and prints give useful reference

--- Be-states/antisymm.c
+++ Be-states/antisymm.c
@@ -24,68 +24,76 @@
 
 #include <stdio.h>        // fprintf(stderr, "...")  Can be eliminated when debugged
 
-void antisymmetrize(Double *tensor, PyInt dim, PyInt n_dim, PyInt** old_orderings, PyInt old_n_orderings, PyInt old_len_orderings, PyInt* strides, PyInt p_0, PyInt* old_phases, PyInt indent)
+// This takes a tensor with an arbitrary number of axes (all of the same length), whereby the only
+// meaningful elements are those where latter indices all have larger values than any of the former.
+// The "elements" of the tensor may themselves be vectors.  When this function runs, it completes
+// the tensor in an antisymmetric way by copying phased versions of the meaningful values.
+//
+void antisymmetrize(Double* tensor,               // the input/output tensor
+                    PyInt   num_axes,             // the number of tensor axes
+                    PyInt   len_axis,             // the length of the axes
+                    PyInt*  strides,              // the vector length of the "elements" of the tensor (given as a reference!))
+                    PyInt   p_0,                  // for recursive use.  0 on first input
+                    PyInt   old_len_orderings,    // for recursive use.  0 on first input
+                    PyInt   old_num_orderings,    // for recursive use.  1 on first input
+                    PyInt** old_orderings,        // for recursive use.  NULL on first input
+                    PyInt*  old_phases)           // for recursive use.  [1] on first input
     {
-    for (int x=0; x<indent; x++) {printf("  ");}
-    printf("p_0 = %d\n", p_0);
+    PyInt len_orderings = old_len_orderings + 1;                // Dimensions of the array that holds ...
+    PyInt num_orderings = old_num_orderings * len_orderings;    // ... different ordering of fixed indices.
 
-    PyInt len_orderings = old_len_orderings + 1;
-    PyInt n_orderings   = old_n_orderings * len_orderings;
+    PyInt new_strides[len_orderings+1];                                       // This array translates multidimensional ...
+    for (PyInt i=len_orderings; i>0; i--) {new_strides[i] = strides[i-1];}    // ... indices to linear index of 1-D array.
+    new_strides[0] = strides[0] * len_axis;                                   // For API convenience, it is one iteration ahead.
 
-    PyInt  orderings_x[n_orderings][len_orderings];
-    PyInt* orderings[n_orderings];
-    for (PyInt m=0; m<n_orderings; m++) {orderings[m] = orderings_x[m];}
+    PyInt  orderings_x[num_orderings][len_orderings];                         // All this to keep pointers ...
+    PyInt* orderings[num_orderings];                                          // ... compatible without a malloc call.
+    for (PyInt m=0; m<num_orderings; m++) {orderings[m] = orderings_x[m];}    // Storage for the orderings of fixed indices.
 
-    PyInt* insert[n_orderings];
-    PyInt  phases[n_orderings];
-
-    PyInt new_strides[len_orderings + 1];
-    for (PyInt i=0; i<len_orderings; i++) {new_strides[i] = strides[i];}
-    new_strides[len_orderings] = new_strides[len_orderings-1] * dim;
-
-    PyInt m = 0;
-    for (PyInt n=0; n<old_n_orderings; n++)
+    PyInt  phases[num_orderings];    // Phase associated with each ordering (relative to indices in ascending order.
+    PyInt* insert[num_orderings];    // Keep track of places where new variable index (looped over in this layer) will go.
+    PyInt m = 0;    // running index for new orderings
+    for (PyInt n=0; n<old_num_orderings; n++)    // For each of the old orderings ...
         {
         PyInt phase = 1;
-        for (PyInt i=len_orderings-1; i>=0; i--)
+        for (PyInt i=len_orderings-1; i>=0; i--)    // ... insert the new index in each possible location.
             {
-            phases[m] = old_phases[n] * phase;
+            phases[m] = phase * old_phases[n];
             phase *= -1;
-            insert[m] = &(orderings[m][i]);
-            orderings[m][i] = -13;
-            for (PyInt j=0;   j<i;             j++) {orderings[m][j] = old_orderings[n][j];}
-            for (PyInt j=i+1; j<len_orderings; j++) {orderings[m][j] = old_orderings[n][j-1];}
+            insert[m] = &(orderings[m][i]);         // can efficiently fill in these blanks later
+            for (PyInt j=0;   j<i;             j++) {orderings[m][j] = old_orderings[n][j];}      // These parts are constant during ...
+            for (PyInt j=i+1; j<len_orderings; j++) {orderings[m][j] = old_orderings[n][j-1];}    // ... looping below.
             m++;
             }
         }
 
-    for (PyInt p=p_0; p<dim; p++)
+    for (PyInt p=p_0; p<len_axis; p++)    // loop over the next index
         {
-        for (PyInt m=0; m<n_orderings; m++) {insert[m][0] = p;}
-        if (len_orderings < n_dim)
+        for (PyInt m=0; m<num_orderings; m++) {insert[m][0] = p;}    // fill in its location in the orderings
+        if (len_orderings < num_axes)     // If there is another index, keep going ...
             {
-            antisymmetrize(tensor, dim, n_dim, orderings, n_orderings, len_orderings, new_strides, p+1, phases, indent+1);
+            antisymmetrize(tensor, num_axes, len_axis, new_strides, p+1, len_orderings, num_orderings, orderings, phases);
             }
-        else
+        else                              // ... otherwise it is time to do the copying finally.
             {
-            PyInt idx_0 = 0;
-            for (PyInt i=0; i<len_orderings; i++) {idx_0 += orderings[0][i] * strides[i];}
-            for (PyInt m=1; m<n_orderings; m++)
-               {
-               PyInt idx = 0;
-               for (PyInt i=0; i<len_orderings; i++) {idx += orderings[m][i] * strides[i];}
-               for (PyInt k=0; k<strides[0]; k++) {tensor[idx+k] = phases[m] * tensor[idx_0+k];}
-               }
+            printf("==========\n");
+            PyInt idx_0 = 0;                                                                    // Build the linear index ...
+            for (PyInt i=0; i<len_orderings; i++) {printf("%7d", strides[i]);}
+            printf("\n");
+            for (PyInt i=0; i<len_orderings; i++) {printf("%7d", orderings[0][i]);
+                idx_0 += orderings[0][i] * strides[i];}      // ... for ascending-order case.
+            printf("   ->\n");
+            for (PyInt m=1; m<num_orderings; m++)    // loop over other arrangements of the indices
+                {
+                PyInt idx = 0;                                                                  // Build the linear index ...
+                for (PyInt i=0; i<len_orderings; i++) {printf("%7d", orderings[m][i]);
+                    idx += orderings[m][i] * strides[i];}    // ... for the alternate arrangement.
+                printf("   x %2d  (%d)\n", phases[m], strides[len_orderings-1]);
+                //for (PyInt k=0; k<strides[len_orderings-1]; k++) {tensor[idx+k] = phases[m] * tensor[idx_0+k];}    // Fill in redundant elements (to within a phase).
+                }
+            printf("==========\n");
             }
         }
 
     return;
     }
-
-
-
-
-
-
-
-
--- Be-states/drive.py
+++ Be-states/drive.py
@@ -7,7 +7,15 @@ Ddummy = numpy.zeros(1, dtype=Double.numpy)
 Idummy = numpy.zeros(1, dtype=BigInt.numpy)
 
 def test_it():
-    antisymm.antisymmetrize(Ddummy, 5, 4, [Idummy], 1, 0, [10], 0, [1], 0)
+    antisymm.antisymmetrize(Ddummy,      # the input/output tensor
+                            4,           # the number of tensor axes
+                            5,           # the length of the axes
+                            [10],        # the vector length of the "elements" of the tensor (given as a reference!)
+                            0,           # for recursive use.  0 on first input
+                            0,           # for recursive use.  0 on first input
+                            1,           # for recursive use.  1 on first input
+                            [Idummy],    # for recursive use.  NULL on first input
+                            [1])         # for recursive use.  [1] on first input
 
 test_it()
 
