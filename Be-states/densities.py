f7006770810c01ed49578f7caedf0fa98a31e2e3
Anthony D. Dutoi <adutoi@tengri>
2024-06-17 11:03:29 -0700

    added capability to compress density tensors in bases of bra and ket natural orbitals

--- Be-states/densities.py
+++ Be-states/densities.py
@@ -18,7 +18,8 @@
 
 import numpy
 import tensorly
-from qode.math.tensornet import tl_tensor, tensor_sum, raw
+from qode.util           import sort_eigen
+from qode.math.tensornet import tl_tensor, tensor_sum, raw, evaluate
 from qode.math           import svd_decomposition
 import field_op
 
@@ -70,32 +71,48 @@ def build_tensors(states, n_orbs, n_elec_0, thresh=1e-10, options=None, n_thread
         "ccaaa": {+1:[(0,1,2,3,4), (0,1,3,4,2), (0,1,4,2,3), (1,0,2,4,3), (1,0,3,2,4), (1,0,4,3,2)], -1:[(0,1,2,4,3), (0,1,3,2,4), (0,1,4,3,2), (1,0,2,3,4), (1,0,3,4,2), (1,0,4,2,3)]},
         "cccaa": {+1:[(0,1,2,3,4), (1,2,0,3,4), (2,0,1,3,4), (0,2,1,4,3), (1,0,2,4,3), (2,1,0,4,3)], -1:[(0,2,1,3,4), (1,0,2,3,4), (2,1,0,3,4), (0,1,2,4,3), (1,2,0,4,3), (2,0,1,4,3)]},
     }
-
     densities = {}
+
+    print("Computing densities ...")
 
     antisymm_numerical = not options("antisymm", "abstract")
 
-    print("Computing densities ...")
     for bra_chg in states:
-        print(bra_chg)
         bra_coeffs  = states[bra_chg].coeffs
         bra_configs = field_op.packed_configs(states[bra_chg].configs)
         for ket_chg in states:
-            print("  ", ket_chg)
             ket_coeffs  = states[ket_chg].coeffs
             ket_configs = field_op.packed_configs(states[ket_chg].configs)
             chg_diff = bra_chg - ket_chg
             if chg_diff in op_strings:
                 for op_string in op_strings[chg_diff]:
                     if op_string not in densities:  densities[op_string] = {}
-                    print(op_string, bra_chg, ket_chg)
+                    print(bra_chg, ket_chg, op_string)
                     rho = field_op.build_densities(op_string, n_orbs, bra_coeffs, ket_coeffs, bra_configs, ket_configs, thresh, wisdom=None, antisymmetrize=antisymm_numerical, n_threads=n_threads)
                     densities[op_string][bra_chg,ket_chg] = [[_tens_wrap(rho_ij) for rho_ij in rho_i] for rho_i in rho]
 
+    print("Postprocessing ...")
+
+    natorbs = options("natorbs")
+
+    if natorbs:
+        natural_orbs = {}
+        for chg,_ in densities["ca"]:
+            rho = densities["ca"][chg,chg]              # bra/ket charges must be the same for this string ...
+            natural_orbs_chg = []
+            for i in range(len(rho)):                   # ... which means the number of bras and kets are the same
+                rho_ii = numpy.array(raw(rho[i][i]))
+                #print(chg, i, "deviation from symmetric:", numpy.linalg.norm(rho_ii - rho_ii.T))
+                evals, evecs = sort_eigen(numpy.linalg.eigh(rho_ii), order="descending")
+                natural_orbs_chg += [_tens_wrap(evecs)]
+            natural_orbs[chg] = natural_orbs_chg
+
     svd = not options("nosvd")
 
-    print("Postprocessing ...")
     for op_string in densities:
+        c_count = op_string.count("c")
+        a_count = op_string.count("a")
+        indices = list(range(len(op_string)))
         for bra_chg,ket_chg in densities[op_string]:
             print(op_string, bra_chg, ket_chg)
             rho = densities[op_string][bra_chg,ket_chg]
@@ -105,10 +122,33 @@ def build_tensors(states, n_orbs, n_elec_0, thresh=1e-10, options=None, n_thread
             for i,rho_i in enumerate(rho):
                 n_kets = len(rho_i)
                 for j,rho_ij in enumerate(rho_i):
-                    indices = list(range(len(op_string)))
+                    #
+                    if natorbs:
+                        p = 0
+                        for _ in range(c_count):
+                            indices_p = list(indices)
+                            indices_p[p] = "p"
+                            rho_ij = rho_ij(*indices_p) @ natural_orbs[bra_chg][i]("p",p)
+                            p += 1
+                        for _ in range(a_count):
+                            indices_p = list(indices)
+                            indices_p[p] = "p"
+                            rho_ij = rho_ij(*indices_p) @ natural_orbs[ket_chg][j]("p",p)
+                            p += 1
                     if svd:
-                        c_count = op_string.count("c")
-                        rho_ij = svd_decomposition(raw(rho_ij), indices[:c_count], indices[c_count:], wrapper=_tens_wrap)
+                        rho_ij = svd_decomposition(numpy.array(raw(rho_ij)), indices[:c_count], indices[c_count:], wrapper=_tens_wrap)
+                    if natorbs:
+                        p = 0
+                        for _ in range(c_count):
+                            indices_p = list(indices)
+                            indices_p[p] = "p"
+                            rho_ij = rho_ij(*indices_p) @ natural_orbs[bra_chg][i](p,"p")
+                            p += 1
+                        for _ in range(a_count):
+                            indices_p = list(indices)
+                            indices_p[p] = "p"
+                            rho_ij = rho_ij(*indices_p) @ natural_orbs[ket_chg][j](p,"p")
+                            p += 1
                     if not antisymm_numerical:
                         if op_string in permutations:
                             temp_ij = tensor_sum()
@@ -117,10 +157,13 @@ def build_tensors(states, n_orbs, n_elec_0, thresh=1e-10, options=None, n_thread
                             for permutation in permutations[op_string][-1]:
                                 temp_ij -= rho_ij(*permutation)
                             rho_ij = temp_ij
+                    #
                     temp += _vec(i,n_bras)(0) @ _vec(j,n_kets)(1) @ rho_ij(*(p+2 for p in indices))
             #
             densities[op_string][bra_chg,ket_chg] = temp
 
     densities["n_elec"]   = {chg:(n_elec_0-chg)          for chg in states}
     densities["n_states"] = {chg:len(states[chg].coeffs) for chg in states}
+
+    print("Writing to hard drive ...")
     return densities
